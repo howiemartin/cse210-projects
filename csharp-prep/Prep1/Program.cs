746ac1c5b99b963f61d5cd225c08fd368853baf1
Howard Martin <mar25005@byui.edu>
2026-05-10 21:48:11 -0600

    Completed Assignments.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string hmfirst = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string hmlast = Console.ReadLine();
+        Console.WriteLine($"\nYour name is {hmlast}, {hmfirst} {hmlast}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Input the percent grade: ");
+        string hminputGrade = Console.ReadLine();
+        int hmpercentGrade = int.Parse(hminputGrade);
+        string hmletter;
+        bool hmpass = false;
+        if (hmpercentGrade >= 90)
+        {
+            hmletter = "A";
+            hmpass = true;
+        }
+        else if (hmpercentGrade >= 80)
+        {
+            hmletter = "B";
+            hmpass = true;
+        }
+        else if (hmpercentGrade >= 70)
+        {
+            hmletter = "C";
+            hmpass = true;
+        }
+        else if (hmpercentGrade >= 60)
+        {
+            hmletter = "D";
+        }
+        else
+        {
+            hmletter = "F";
+        }
+        Console.WriteLine($"Your grade is {hmletter}.");
+        if (hmpass == true)
+        {
+            Console.WriteLine("Congrats! You passed.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you failed. Better luck next time!");
+        }
     }
 }
