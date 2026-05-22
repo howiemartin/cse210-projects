8c65b4d436100c9bf72cb125b1847c52166531c2
Howard Martin <mar25005@byui.edu>
2026-05-21 18:23:52 -0600

    completed prep materials #4 and #5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Globalization;
+using System.Xml.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int hmnumb = -1;
+        List<int> hmnumbs = new List<int>();
+
+        while (hmnumb != 0 )
+        {
+            Console.WriteLine("Enter a number:");
+            hmnumb = int.Parse(Console.ReadLine());
+            if (hmnumb != 0)
+            {
+                hmnumbs.Add(hmnumb);
+            }
+            
+        }
+
+        float hmsum = hmnumbs.Sum();
+        Console.WriteLine($"Sum: {hmsum}");
+        
+        int hmlargest = 0;
+        int hmlistSize = hmnumbs.Count();
+        foreach (int number in hmnumbs)
+        {
+            if (number > hmlargest)
+            {
+                hmlargest = number;
+            }
+        }
+        float hmaverage = hmsum / hmlistSize;
+        Console.WriteLine($"Average: {hmaverage}");
+        Console.WriteLine($"Largest: {hmlargest}");
+        
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        string PromptUserName()
+        {
+            Console.WriteLine("Enter your name: ");
+            return Console.ReadLine();
+        }
+        int PromptUserNumber()
+        {
+            Console.WriteLine("Enter your favorite number: ");
+            return int.Parse(Console.ReadLine());
+        }
+        
+        void PromptUserBirthYear(out int hmyear)
+        {
+            Console.WriteLine("Enter your birth year: ");
+            hmyear = int.Parse(Console.ReadLine());
+        }
+        int SquareNumber(int x)
+        {
+            return x * x;
+        }
+        void DisplayResult()
+        {
+           DisplayWelcome();
+           string hmname = PromptUserName();
+           int hmnumb = PromptUserNumber();
+           PromptUserBirthYear(out int hmyear);
+           Console.WriteLine($"{hmname}, the square of your number is {SquareNumber(hmnumb)}.\n{hmname}, you will turn {2026 - hmyear} this year.");
+        }
+DisplayResult();
+
+
+
+
+        
     }
 }
